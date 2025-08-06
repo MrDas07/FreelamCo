a9b71f4c6e94a688a7ff46cf7f8e9f0e801d0c81
MrDas07 <sankardas1627@gmail.com>
2025-08-06 20:08:16 +0530

    Initial commit with backend server

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,9 @@ const { registerUser, loginUser } = require('../controllers/userController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/', (req, res) => {
+    res.json({ message: 'User routes are working!' });
+  });
 
 
 module.exports = router;
